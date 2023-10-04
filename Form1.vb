cc6f17549707dce3b27d8155fc5b564a62ac2513
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-04 21:09:21 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim text1, text2, text3, text4 As String
+
+        text1 = "VISUAL"
+        text2 = "BASIC"
+        text3 = "2022"
+
+        text4 = text1 + "" + text2 + "" + text3
+
+        MsgBox(text4)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
